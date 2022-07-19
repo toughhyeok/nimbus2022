fe32932c3778594ee14c901e79f4a85dcf14922e
junhyeok <toughhyeok96@gmail.com>
2022-07-19 22:56:30 +0900

    fix typo (is_actice -> is_active)

--- app/core/models.py
+++ app/core/models.py
@@ -36,7 +36,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_actice = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
